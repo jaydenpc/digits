6e4af574a07fd16281daca89a643f336ba838acf
Jayden Cruz <jaydencruz1121@gmail.com>
2026-04-06 20:39:00 -1000

    Cleaned up

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -23,20 +23,7 @@ async function main() {
     });
     // console.log(`  Created user: ${user.email} with role: ${user.role}`);
   });
-  for (const data of config.defaultData) {
-    const condition = data.condition as Condition || Condition.good;
-    console.log(`  Adding stuff: ${JSON.stringify(data)}`);
-    await prisma.stuff.upsert({
-      where: { id: config.defaultData.indexOf(data) + 1 },
-      update: {},
-      create: {
-        name: data.name,
-        quantity: data.quantity,
-        owner: data.owner,
-        condition,
-      },
-    });
-  }
+
   console.log('  Creating contacts...');
 
 for (const contact of config.defaultContacts) {
